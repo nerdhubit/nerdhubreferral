2915c3a8e811c4f65623c19b710ecbdae71a9635
nerdhubit <45185899+nerdhubit@users.noreply.github.com>
2018-11-20 22:24:45 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://www.nerdhub.it/telegrambot/execute.php';
+$BOT_TOKEN = '792894187:AAG8OLY3I8U0byw3F7N9kGvdlANg5a67PRU';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
